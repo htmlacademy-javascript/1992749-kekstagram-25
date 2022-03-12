3e623103c76773faf0f0b8085bf4ed5ffbb30af8
LevDiakov <381208dl@gmail.com>
2022-03-12 14:14:40 +0300

    Домашнее задание: 7.10. Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import './setup.js'; // это файл для настроек загрузки фото, фильтации, редактирования и т.д.
+import './miniature.js'; // это файл для  отрисовки миниатюр
 import {createPhoto} from './data.js';
 createPhoto();
+//console.log(createPhoto());
